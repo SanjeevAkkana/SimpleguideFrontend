53ba0318cfed17e95fb289e1a4111f2af2aecb76
SanjeevAkkana <118372545+SanjeevAkkana@users.noreply.github.com>
2023-08-22 22:52:56 +0530

    Update Blog.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -194,7 +194,7 @@ const Blog = () => {
               </div>
               {/* Questions */}
               {
-                blog.questions>1
+                blog.questions.length>1
                   ?
                   <div>
                     <p className='text-2xl font-semibold py-6'>Frequently asked question :-</p>
